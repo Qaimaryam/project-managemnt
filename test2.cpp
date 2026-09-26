441212c096553c0ba82283ab2856d5bd3332ade1
Qaimaryam <qaimaryam450@gmail.com>
2026-09-26 10:58:20 +0500

    Added failing test for announcement notification

--- test2.cpp
+++ test2.cpp
@@ -1,53 +1,53 @@
-#include <iostream>
-#include <string>
-#include <cassert>
-using namespace std;
-
-struct Notification {
-    string userId;
-    string type;
-    string message;
-};
-
-Notification* inbox = new Notification[100];
-int inboxCount = 0;
-
-string* enrolledStudents = new string[3]{ "Ali", "Sara", "Zain" };
-int studentCount = 3;
-
-void NotifyUser(string userId, string type, string message) {
-    inbox[inboxCount].userId = userId;
-    inbox[inboxCount].type = type;
-    inbox[inboxCount].message = message;
-    inboxCount++;
-}
-
-void NotifyAllStudents(string type, string message) {
-    for (int i = 0; i < studentCount; i++) {
-        NotifyUser(enrolledStudents[i], type, message);
-    }
-}
-
-bool CancelClass(string classId) {
-    NotifyAllStudents("Cancellation", "Class " + classId + " has been cancelled");
-    return true;
-}
-
-void TestCancelClass_NotifiesStudents() {
-    bool cancelled = CancelClass("CS101");
-    assert(cancelled == true);
-
-    bool found = false;
-    for (int i = 0; i < inboxCount; i++) {
-        if (inbox[i].userId == "Ali" && inbox[i].type == "Cancellation") {
-            found = true;
-        }
-    }
-    assert(found == true);
-    cout << "TestCancelClass_NotifiesStudents PASSED\n";
-}
-
-int main() {
-    TestCancelClass_NotifiesStudents();
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <cassert>
+//using namespace std;
+//
+//struct Notification {
+//    string userId;
+//    string type;
+//    string message;
+//};
+//
+//Notification* inbox = new Notification[100];
+//int inboxCount = 0;
+//
+//string* enrolledStudents = new string[3]{ "Ali", "Sara", "Zain" };
+//int studentCount = 3;
+//
+//void NotifyUser(string userId, string type, string message) {
+//    inbox[inboxCount].userId = userId;
+//    inbox[inboxCount].type = type;
+//    inbox[inboxCount].message = message;
+//    inboxCount++;
+//}
+//
+//void NotifyAllStudents(string type, string message) {
+//    for (int i = 0; i < studentCount; i++) {
+//        NotifyUser(enrolledStudents[i], type, message);
+//    }
+//}
+//
+//bool CancelClass(string classId) {
+//    NotifyAllStudents("Cancellation", "Class " + classId + " has been cancelled");
+//    return true;
+//}
+//
+//void TestCancelClass_NotifiesStudents() {
+//    bool cancelled = CancelClass("CS101");
+//    assert(cancelled == true);
+//
+//    bool found = false;
+//    for (int i = 0; i < inboxCount; i++) {
+//        if (inbox[i].userId == "Ali" && inbox[i].type == "Cancellation") {
+//            found = true;
+//        }
+//    }
+//    assert(found == true);
+//    cout << "TestCancelClass_NotifiesStudents PASSED\n";
+//}
+//
+//int main() {
+//    TestCancelClass_NotifiesStudents();
+//    return 0;
+//}
---
+++ test3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+struct Notification {
+    string userId;
+    string type;
+    string message;
+};
+
+Notification* inbox = new Notification[100];
+int inboxCount = 0;
+
+string* enrolledStudents = new string[3]{ "Ali", "Sara", "Zain" };
+int studentCount = 3;
+
+void NotifyUser(string userId, string type, string message) {
+    inbox[inboxCount].userId = userId;
+    inbox[inboxCount].type = type;
+    inbox[inboxCount].message = message;
+    inboxCount++;
+}
+
+void NotifyAllStudents(string type, string message) {
+    for (int i = 0; i < studentCount; i++) {
+        NotifyUser(enrolledStudents[i], type, message);
+    }
+}
+
+bool CancelClass(string classId) {
+    NotifyAllStudents("Cancellation", "Class " + classId + " has been cancelled");
+    return true;
+}
+
+// Sirf declaration - body nahi likhi abhi
+bool SendAnnouncement(string message);
+
+void TestCancelClass_NotifiesStudents() {
+    bool cancelled = CancelClass("CS101");
+    assert(cancelled == true);
+    cout << "TestCancelClass_NotifiesStudents PASSED\n";
+}
+
+void TestSendAnnouncement_ReachesAllStudents() {
+    bool sent = SendAnnouncement("Exam postponed");
+    assert(sent == true);
+
+    int count = 0;
+    for (int i = 0; i < inboxCount; i++) {
+        if (inbox[i].type == "Announcement") count++;
+    }
+    assert(count == studentCount);
+    cout << "TestSendAnnouncement_ReachesAllStudents PASSED\n";
+}
+
+int main() {
+    TestCancelClass_NotifiesStudents();
+    TestSendAnnouncement_ReachesAllStudents();
+    return 0;
+}
